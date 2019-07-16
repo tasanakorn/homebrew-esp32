671df50479fff952b01e028c8bb29ea05d0d9bb0
Tasanakorn P <tasanakorn@gmail.com>
2019-07-16 10:51:57 +0700

    Upgrade to version 2019r1

--- xtensa-esp32-elf.rb
+++ xtensa-esp32-elf.rb
@@ -1,20 +1,16 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class XtensaEsp32Elf < Formula
   desc "ESP32 toolchain"
-  homepage ""
-  url "https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-59.tar.gz"
-  version "1.22.0"
-  sha256 "95abdd14b49056ba63981f590f7e54a54b846ab93dc38b81c1f098a240e1599d"
+  homepage "https://docs.espressif.com/projects/esp-idf/en/latest/get-started/macos-setup.html"
+  url "https://dl.espressif.com/dl/xtensa-esp32-elf-gcc8_2_0-esp32-2019r1-macos.tar.gz"
+  version "2019r1"
+  sha256 "adb256394c948ca424ec6ef1d9bee91baa99a304d8ace8e6701303da952eb007"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink Dir[libexec/"bin/*"]
   end
 
-  # test do
-  #   system "false"
-  # end
+  test do
+    system "#{bin}/xtensa-esp32-elf-gcc", "--version"
+  end
 end
